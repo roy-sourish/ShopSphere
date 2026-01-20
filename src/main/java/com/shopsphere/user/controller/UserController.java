39d1bcc66b4d4958e70efcae609032754cd8b9c7
Sourish Roy <sourish.roy1973@gmail.com>
2026-01-20 17:49:53 +0530

    Add validation to CreateUserRequest and update user API
    Introduced Jakarta Bean Validation annotations to CreateUserRequest for email and password fields, enforcing non-empty and format/length constraints. Updated UserController to use @Valid and changed the base request mapping to '/api/v1/users'. Added spring-boot-starter-validation dependency to pom.xml to support validation.

--- src/main/java/com/shopsphere/user/controller/UserController.java
+++ src/main/java/com/shopsphere/user/controller/UserController.java
@@ -4,11 +4,12 @@ import com.shopsphere.user.domain.User;
 import com.shopsphere.user.dto.CreateUserRequest;
 import com.shopsphere.user.dto.UserResponse;
 import com.shopsphere.user.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/v1/users")
 public class UserController {
     private final UserService userService;
 
@@ -18,7 +19,7 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public UserResponse createUser(@RequestBody CreateUserRequest request){
+    public UserResponse createUser(@Valid @RequestBody CreateUserRequest request){
         User user = userService.createUser(request.getEmail(), request.getPassword());
 
         return  UserResponse.from(user);
--- src/main/java/com/shopsphere/user/dto/CreateUserRequest.java
+++ src/main/java/com/shopsphere/user/dto/CreateUserRequest.java
@@ -1,12 +1,21 @@
 package com.shopsphere.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 /**
  * This class represents: <br>
  * "The shape of the JSON that the client is allowed to send when creating a user." <br>
  * It's an API contract
  * */
 public class CreateUserRequest {
+    @NotBlank(message = "Email must not be empty")
+    @Email(message = "Email must be a valid email address")
     private String email;
+
+    @NotBlank(message = "Password must not be empty")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
     public String getEmail() {
